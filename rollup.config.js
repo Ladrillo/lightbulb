96f015a301886901c99d267c550410b2d1ead754
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-04-12 15:16:42 +0200

    wip

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import { uglify } from 'rollup-plugin-uglify'
+import babel from 'rollup-plugin-babel'
+
+export default {
+  input: 'src/index.js',
+  external: ['react', 'prop-types'],
+  output: {
+    format: 'umd',
+    name: 'lightbulb',
+    globals: {
+      react: "React"
+    }
+  },
+  plugins: [
+    babel({
+      exclude: "node_modules/**"
+    }),
+    // uglify(),
+  ],
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
-import React from 'react'
-import { render } from 'react-dom'
-import Bulb from './Bulb'
-
-
-function Canvas({ }) {
-  return (
-    <div>
-      <Bulb color='red' on size={450} />
-    </div>
-  )
-}
-
-render(
-  <Canvas />,
-  document.querySelector('#root')
-)
+export { default as Module } from './Bulb.jsx'
+// import Bulb from './Bulb.jsx'
+// export default Bulb
